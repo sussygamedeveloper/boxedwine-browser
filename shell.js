6639b942f6e72674578b0833f4c17ec7a152753d
sussygamedeveloper <98538512+sussygamedeveloper@users.noreply.github.com>
2022-03-09 20:16:00 -0500

    Update shell.js

--- shell.js
+++ shell.js
@@ -71,7 +71,7 @@
             Config.extraZipFiles = "";//getZipFileList("overlay"); //MANUAL:"dlls.zip;fonts.zip";
             Config.appZipFile = "files.zip";//getAppZipFile("app"); //MANUAL:"chomp.zip";
             Config.rootZipFile = DEFAULT_ROOT_ZIP_FILE;//getRootZipFile("root"); //MANUAL:"base.zip";
-            Config.Program = "flashplayer_11_sa_32bit.exe";//getExecutable(); //MANUAL:"CHOMP.EXE";
+            Config.Program = "explorer.exe";//getExecutable(); //MANUAL:"CHOMP.EXE";
             Config.WorkingDir = "";//getWorkingDirectory(); //MANUAL:"";
             Config.retrieveDlls = DEFAULT_RETRIEVE_REMOTE_DLL_FILES;//getRetrieveDlls();
             Config.isSoundEnabled = DEFAULT_SOUND_ENABLED;//getSound();
